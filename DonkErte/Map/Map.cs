4287b6423f2826388db90c004a071a287dfa0095
Steve Pletcher <steven.j.pletcher@gmail.com>
2014-02-19 23:02:15 -0500

    Reworked structure a bit, added some events

--- DonkErte/Map/Map.cs
+++ DonkErte/Map/Map.cs
@@ -7,6 +7,15 @@ namespace DonkErte.Map
 {
     public class Map
     {
+        public delegate void MapEventHandler(object sender, EventArgs e);
+
+        public event MapEventHandler Loaded;
+        public event MapEventHandler Unloaded;
         public Tile[,] Squares;
+
+        public Map(int width, int height)
+        {
+            Squares = new Tile[width,height];
+        }
     }
 }
--- DonkErte/Map/Tile.cs
+++ DonkErte/Map/Tile.cs
@@ -5,19 +5,36 @@ using System.Text;
 
 namespace DonkErte.Map
 {
-    public abstract class Tile
+    public class Tile
     {
-        public delegate void TileEventHandler(object sender, TileEventArgs e);
+        public delegate void TileEventHandler(object sender, EventArgs e);
+        public event TileEventHandler InteractedWith;
+        public event TileEventHandler Entered;
         private int _graphicTile;
         private Map _parent;
-        protected Tile(int graphic, Map parent)
+        public bool Enterable;
+        protected Tile(int graphic, Map parent, bool enterable)
         {
             _graphicTile = graphic;
             _parent = parent;
+            Enterable = enterable;
         }
         //public Graphic Render();
 
-        public abstract bool CanPlayerEnter();
+        public void Interact()
+        {
+            if (InteractedWith != null)
+            {
+                InteractedWith.Invoke(this, EventArgs.Empty);
+            }
+        }
 
+        public void OnEnter()
+        {
+            if (Entered != null)
+            {
+                Entered.Invoke(this, EventArgs.Empty);
+            }
+        }
     }
 }
