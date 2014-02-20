f1681536501f43e4f0a1e8036b531b313d02ce22
Steve Pletcher <steven.j.pletcher@gmail.com>
2014-02-19 22:17:32 -0500

    Initial commit

---
+++ DonkErte/DonkErte/Map/Map.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DonkErte.Map
+{
+    public class Map
+    {
+        public Tile[,] Squares;
+    }
+}
---
+++ DonkErte/DonkErte/Map/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DonkErte.Map
+{
+    public abstract class Tile
+    {
+        public delegate void TileEventHandler(object sender, TileEventArgs e);
+        private int _graphicTile;
+        private Map _parent;
+        protected Tile(int graphic, Map parent)
+        {
+            _graphicTile = graphic;
+            _parent = parent;
+        }
+        //public Graphic Render();
+
+        public abstract bool CanPlayerEnter();
+
+    }
+}
